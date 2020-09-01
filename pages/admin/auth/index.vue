90b344a57ef8dabf1d73ae3ee95f189cd2c1972c
Eric Wong <ericwong12@live.com>
2020-09-01 15:54:15 -0600

    Updated styling and functionality of sign in

--- pages/admin/auth/index.vue
+++ pages/admin/auth/index.vue
@@ -1,11 +1,15 @@
  <template>
   <div class="admin-auth-page">
     <div class="auth-container">
+      <h1>Sign in</h1>
       <form @submit.prevent="onSubmit">
         <AppControlInput type="email" v-model="email">E-Mail Address</AppControlInput>
         <AppControlInput type="password" v-model="password">Password</AppControlInput>
         <!-- <p style="color=red">{{}}</p> -->
         <AppButton type="submit">{{ isLogin ? 'Login' : 'Sign Up' }}</AppButton>
+        <AppButton>
+          <nuxt-link to="/">Return Home</nuxt-link>
+        </AppButton>
         <!-- Removed Sign up option -->
         <!-- <AppButton
           type="button"
@@ -48,6 +52,8 @@ export default {
 <style scoped>
 .admin-auth-page {
   padding: 20px;
+  height: 100vh;
+  position: relative;
 }
 
 .auth-container {
@@ -55,8 +61,15 @@ export default {
   border-radius: 5px;
   box-shadow: 0 2px 2px #ccc;
   width: 300px;
-  margin: auto;
+  margin: 0 auto;
   padding: 10px;
   box-sizing: border-box;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+a {
+  color: white;
 }
 </style>
